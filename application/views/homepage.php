fcde3563992db4d24adb10f41e590d5aa6c2eb36
wabrown <crunchymail27@gmail.com>
2017-02-19 19:45:47 -0800

    added helpme page

---
+++ application/views/homepage.php
@@ -0,0 +1,10 @@
+<div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
+				<table class="table">
+					{display_tasks}
+					<tr>
+						<td>{id}</td>
+						<td>{task}</td>
+						<td>{priority}</td>
+					</tr>
+					{/display_tasks}
+				</table>
