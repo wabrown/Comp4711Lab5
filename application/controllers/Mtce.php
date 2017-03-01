7c523c224e7dabe4e86d2d9a06521223d4529c7f
Jonathan De Maeseneer <jldm42@gmail.com>
2017-02-28 16:16:42 -0800

    Job 4 - Maintenance list.

---
+++ application/controllers/Mtce.php
@@ -0,0 +1,25 @@
+<?php
+
+class Mtce extends Application {
+
+        public function index()
+        {
+                $this->data['pagetitle'] = 'TODO List Maintenance';
+                $tasks = $this->tasks->all(); // get all the tasks
+
+                // substitute the status name
+                foreach ($tasks as $task)
+                        if (!empty($task->status))
+                                $task->status = $this->statuses->get($task->status)->name;
+
+                // build the task presentation output
+                $result = '';   // start with an empty array        
+                foreach ($tasks as $task)
+                        $result .= $this->parser->parse('oneitem',(array)$task,true);
+
+                // and then pass them on
+                $this->data['display_tasks'] = $result;
+                $this->data['pagebody'] = 'itemlist';
+                $this->render();
+        }
+}
---
+++ application/views/itemlist.php
@@ -0,0 +1,8 @@
+<table class="table">
+        <tr>
+                <th>Id</th>
+                <th>Task</th>
+                <th>Status</th>
+        </tr>
+        {display_tasks}
+</table>
